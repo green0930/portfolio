22eae3a5e9171b3f9a61d2cf6daead7752f2018a
Haejin Green <atom82@hotmail.com>
2021-05-04 14:28:10 -0700

    first commit

---
+++ build/precache-manifest.2cc4c6e2b4897c75141520b78ed9dfa5.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f45f6a4ca98637ba5212312ca7b125b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3c3bf5ee6ef65611ff3",
+    "url": "/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "059db7f909e4d3376d11",
+    "url": "/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "0f01e4732c3114d2fbcc",
+    "url": "/static/css/main.207db4cf.chunk.css"
+  },
+  {
+    "revision": "99ed837f0a5599e9ed2c",
+    "url": "/static/js/2.8e5a6e7a.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/static/js/2.8e5a6e7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3c3bf5ee6ef65611ff3",
+    "url": "/static/js/3.bef3d781.chunk.js"
+  },
+  {
+    "revision": "059db7f909e4d3376d11",
+    "url": "/static/js/4.2763dfb1.chunk.js"
+  },
+  {
+    "revision": "0f01e4732c3114d2fbcc",
+    "url": "/static/js/main.0c66019b.chunk.js"
+  },
+  {
+    "revision": "65312a03bf5abf80839a",
+    "url": "/static/js/runtime-main.6393f98f.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "cde01cb45507fb5b95e56ec9de8a2df8",
+    "url": "/static/media/dkulogo.cde01cb4.png"
+  },
+  {
+    "revision": "c1cbe37e1f09bc8e1f0c09a2dbbabfa0",
+    "url": "/static/media/jounlogo.c1cbe37e.webp"
+  },
+  {
+    "revision": "1d20ae2b0d86f8f1d89e6d41b6b4d9c2",
+    "url": "/static/media/kenzielogo.1d20ae2b.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "4122433dbbd0012e6477667babdba19f",
+    "url": "/static/media/samhologo.4122433d.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
---
+++ src/portfolio.js
@@ -0,0 +1,370 @@
+﻿/* Change this file to get your personal Portfolio */
+
+// Summary And Greeting Section
+
+import emoji from "react-easy-emoji"
+
+const illustration = {
+  animated: true, // set to false to use static SVG
+}
+
+const greeting = {
+  username: "Haejin Green",
+  title: "Hi all, I'm Haejin",
+  subTitle: emoji(
+    "Self-motivated learner who has passionate to become a devloper. \
+    A student of software engineering at Kenzie Academy, fast learner, detail-oriented person who is ready to change a journey to tech industry.\
+    Wiling to develop my experience with new skill"
+  ),
+  resumeLink:
+    "https://docs.google.com/document/d/1a6MCVLiO0VoOWUQCQ5GaWpS3ubgCCW3mi8Jg3YG38Rk/edit#",
+  displayGreeting: true, // Set false to hide this section, defaults to true
+}
+
+// Social Media Links
+
+const socialMediaLinks = {
+  github: "https://github.com/green0930",
+  linkedin: "https://www.linkedin.com/in/haejin-green-08ba5515b/",
+  gmail: "clee8501@gmail.com",
+  gitlab: "https://gitlab.com/green0930",
+  facebook: "https://www.facebook.com/haejin.lee.7528/",
+  // medium: "https://medium.com/@davidrakosi",
+  // stackoverflow: "https://stackoverflow.com/users/13263979/drakosi",
+  instagram: "https://www.instagram.com/hgreen_mom/",
+  twitter: "https://twitter.com/chloelee85",
+  display: true, // Set true to display this section, defaults to false
+}
+
+// Skills Section
+
+const skillsSection = {
+  title: "What I do",
+  subTitle:
+    "efficient Full Stack Software developer who wants to learn all tech skills",
+  skills: [
+    emoji(
+      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+    ),
+    emoji("⚡ Progressive Web Applications ( PWA )"),
+    emoji(
+      "⚡ Integration of third party services such as Firebase/ Digital Ocean"
+    ),
+    emoji("⚡ Develop fully functional backend and database systems"),
+  ],
+
+  /* Make Sure to include correct Font Awesome Classname to view your icon
+https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "fab fa-html5",
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "fab fa-css3-alt",
+    },
+    {
+      skillName: "bootstrap",
+      fontAwesomeClassname: "fab fa-bootstrap",
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "fab fa-js",
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "fab fa-react",
+    },
+    {
+      skillName: "nodejs",
+      fontAwesomeClassname: "fab fa-node",
+    },
+    {
+      skillName: "python",
+      fontAwesomeClassname: "fab fa-python",
+    },
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "fab fa-npm",
+    },
+    {
+      skillName: "non-sql-database",
+      fontAwesomeClassname: "fas fa-database",
+    },
+    {
+      skillName: "firebase",
+      fontAwesomeClassname: "fas fa-fire",
+    },
+  ],
+  display: true, // Set false to hide this section, defaults to true
+}
+
+// Education Section
+
+const educationInfo = {
+  display: true, // Set false to hide this section, defaults to true
+  schools: [
+    {
+      schoolName: "Kenzie Academy",
+      logo: require("./assets/images/kenzielogo.webp"),
+      subHeader: "Full-Stack Software Engineering Program",
+      duration: "July 2020 - Present(Graduate on July 2021)",
+      desc: "Project based program to get cerify as a software engineer",
+      descBullets: [
+        "Front-End: HTML/CSS/JS/MERN STACK",
+        "Back-End: PYTHON/DJANGO",
+      ],
+    },
+    {
+      schoolName: "Dankook University in South Korea",
+      logo: require("./assets/images/dkulogo.png"),
+      subHeader: "Bachelor of Art",
+      duration: "March 2003 - Feburary 2008",
+      desc: "Major: English Literature",
+    },
+  ],
+}
+
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: true, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: "Frontend / Design", //Insert stack or technology you have experience in
+      progressPercentage: "90%", //Insert relative proficiency in percentage
+    },
+    {
+      Stack: "Backend",
+      progressPercentage: "70%",
+    },
+    {
+      Stack: "Programming",
+      progressPercentage: "90%",
+    },
+  ],
+  displayCodersrank: true, // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
+}
+
+// Work experience section
+
+const workExperiences = {
+  display: true, //Set it to true to show workExperiences Section
+  experience: [
+    {
+      role: "Senior Opertation Manager",
+      company: "Samho Tour",
+      companylogo: require("./assets/images/samhologo.webp"),
+      date: "July 2018 – March 2020",
+      desc:
+        "Manage a merchandise of $5M+ of tour programs in booked revenue per Quarter across Asia(Korean)clients.",
+      descBullets: ["Management", "Organization", "Estimated", "Develpoment"],
+    },
+    {
+      role: "Assistant General Manager",
+      company: "Joun World Travel",
+      companylogo: require("./assets/images/jounlogo.webp"),
+      date: "Feb 2017 – June 2018",
+      desc:
+        "Responsible for communicating with executives, internal team and all external business contact with professionalism",
+    },
+    {
+      role: "Operation Manager",
+      company: "Samho Tour",
+      companylogo: require("./assets/images/samhologo.webp"),
+      date: "August 2012 – January 2017",
+      desc:
+        "Estimated quotation, assisted in revenue tracking date entry and provided marketing support",
+    },
+  ],
+}
+
+/* Your Open Source Section to View Your Github Pinned Projects
+To know how to get github key look at readme.md */
+
+const openSource = {
+  githubConvertedToken: process.env.REACT_APP_GITHUB_TOKEN,
+  githubUserName: "green0930", // Change to your github username to view your profile in Contact Section.
+  showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
+  display: true, // Set false to hide this section, defaults to true
+}
+
+// Some big projects you have worked on
+
+const bigProjects = {
+  title: "Big Projects",
+  subtitle: "Capston project with MERN Stack at Kenzie Academy",
+  projects: [
+    {
+      image: require("./assets/images/saayaHealthLogo.webp"),
+      projectName: "Omni Cloud",
+      projectDesc:
+        "Social App for any people who has own passion of their life",
+      footerLink: [
+        {
+          name: "Omni Cloud",
+          url: "fa",
+        },
+        //  you can add extra buttons here.
+      ],
+    },
+    {
+      image: require("./assets/images/nextuLogo.webp"),
+      projectName: "Nextu",
+      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "",
+        },
+      ],
+    },
+  ],
+  display: false, // Set false to hide this section, defaults to true
+}
+
+// Achievement Section
+// Include certificates, talks etc
+
+const achievementSection = {
+  title: emoji("Achievements And Certifications 🏆 "),
+  subtitle:
+    "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
+
+  achievementsCards: [
+    // {
+    //   title: "Google Code-In Finalist",
+    //   subtitle:
+    //     "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
+    //   image: require("./assets/images/codeInLogo.webp"),
+    //   footerLink: [
+    //     {
+    //       name: "Certification",
+    //       url: "",
+    //     },
+    //     {
+    //       name: "Award Letter",
+    //       url: "",
+    //     },
+    //     {
+    //       name: "Google Code-in Blog",
+    //       url: "",
+    //     },
+    //   ],
+    // },
+    {
+      title: "Complete Python Developer 2021, Zero To Mastery",
+      subtitle: " Completed Certification Python from Zero To Mastery Academy",
+      image: require("./assets/images/zmtlogo.png"),
+      footerLink: [
+        {
+          name: "Certificate Of Completion",
+          url:
+            "https://www.udemy.com/certificate/UC-48ae985f-384d-4004-8e51-3796ad884375/",
+        },
+      ],
+    },
+
+    {
+      title: "Front-end Web development",
+      subtitle:
+        "Completed Certifcation from Kenzie Academy for Front-end Web Development",
+      image: require("./assets/images/kenzielogo.webp"),
+      footerLink: [
+        {
+          name: "Certificate of Front-End Web Development",
+          url:
+            "https://drive.google.com/file/d/1tQ11Wk5-ffsoNcrepwgKugGoWOKOvAyh/view?usp=sharing",
+        },
+      ],
+    },
+  ],
+  display: true, // Set false to hide this section, defaults to true
+}
+
+// Blogs Section
+
+const blogSection = {
+  title: "Blogs",
+  subtitle:
+    "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
+
+  blogs: [
+    {
+      url:
+        "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
+      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+      description:
+        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?",
+    },
+    {
+      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
+      title: "Why REACT is The Best?",
+      description:
+        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies.",
+    },
+  ],
+  display: false, // Set false to hide this section, defaults to true
+}
+
+// Talks Sections
+
+const talkSection = {
+  title: "TALKS",
+  subtitle: emoji(""),
+
+  talks: [
+    {
+      title: "",
+      subtitle: "",
+      slides_url: "",
+      event_url: "",
+    },
+  ],
+  display: false, // Set false to hide this section, defaults to true
+}
+
+// Podcast Section
+
+const podcastSection = {
+  title: emoji("Podcast 🎙️"),
+  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
+
+  // Please Provide with Your Podcast embeded Link
+  podcast: [""],
+  display: false, // Set false to hide this section, defaults to true
+}
+
+const contactInfo = {
+  title: emoji("Contact Me ☎️"),
+  subtitle: "CONTACT ME AND HELP ME ACHIEVE MY GOALS AS A SOFTWARE DEVELOPER.",
+  number: "(213) 265 6609",
+  email_address: "atom82@hotmail.com",
+}
+
+// Twitter Section
+
+const twitterDetails = {
+  userName: "chloelee85", //Replace "twitter" with your twitter username without @
+  display: true, // Set true to display this section, defaults to false
+}
+
+export {
+  illustration,
+  greeting,
+  socialMediaLinks,
+  skillsSection,
+  educationInfo,
+  techStack,
+  workExperiences,
+  openSource,
+  bigProjects,
+  achievementSection,
+  blogSection,
+  talkSection,
+  podcastSection,
+  contactInfo,
+  twitterDetails,
+}
